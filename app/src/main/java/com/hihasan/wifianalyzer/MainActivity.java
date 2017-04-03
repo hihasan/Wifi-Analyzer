522385eed37e119011426563fbd72e1621c48138
hihsan <hasan@hihasan.com>
2017-04-03 11:18:21 +0600

    Add Class

--- app/src/main/java/com/hihasan/wifianalyzer/MainActivity.java
+++ app/src/main/java/com/hihasan/wifianalyzer/MainActivity.java
@@ -1,5 +1,6 @@
 package com.hihasan.wifianalyzer;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -12,6 +13,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.hihasan.wifianalyzer.contourgraph.ContourMain;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -84,7 +87,8 @@ public class MainActivity extends AppCompatActivity
 
         if (id == R.id.nav_contour)
         {
-
+            Intent n=new Intent(MainActivity.this, ContourMain.class);
+            startActivity(n);
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
