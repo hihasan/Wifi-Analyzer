aed11cbc2ca72846d6b1cdfd2b0a42f85b0f9089
hihsan <hasan@hihasan.com>
2017-04-03 11:52:26 +0600

    wifi list

--- app/src/main/java/com/hihasan/wifianalyzer/MainActivity.java
+++ app/src/main/java/com/hihasan/wifianalyzer/MainActivity.java
@@ -1,10 +1,13 @@
 package com.hihasan.wifianalyzer;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -12,30 +15,48 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.hihasan.wifianalyzer.contourgraph.ContourMain;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements NavigationView.OnNavigationItemSelectedListener, View.OnClickListener
+{
+    private WifiManager wifi;
+    private ListView lv;
+    private TextView textStatus;
+    private Button buttonScan;
+    private List<ScanResult> results;
+
+    private String ITEM_KEY = "key";
+    private List<HashMap<String, String>> networkList = new ArrayList<HashMap<String, String>>();
+    private SimpleAdapter adapter;
+
+//    String ITEM_KEY = "key";
+//    ArrayList<HashMap<String, String>> arraylist = new ArrayList<HashMap<String, String>>();
+//    SimpleAdapter adapter;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View view)
-            {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        textStatus = (TextView) findViewById(R.id.textStatus);
+        buttonScan = (Button) findViewById(R.id.buttonScan);
+        buttonScan.setOnClickListener(this);
+        lv = (ListView) findViewById(R.id.list);
+
+
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -45,6 +66,40 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        wifi = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+        if (wifi.isWifiEnabled() == false) {
+            Toast.makeText(getApplicationContext(), "wifi is disabled..making it enabled",
+                    Toast.LENGTH_LONG).show();
+            wifi.setWifiEnabled(true);
+        }
+        this.adapter = new SimpleAdapter(MainActivity.this, networkList, R.layout.row,
+                new String[] { ITEM_KEY }, new int[] { R.id.list_value });
+        lv.setAdapter(this.adapter);
+
+        registerReceiver(new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context c, Intent intent) {
+                results = wifi.getScanResults();
+                textStatus.setText(results.size() + " networks found");
+                for (ScanResult result : results) {
+                    HashMap<String, String> items = new HashMap<String, String>();
+                    String itenName = "BSSID=" + result.BSSID + ", SSID=" + result.SSID
+                            + ", capabilities=" + result.capabilities;
+                    items.put(ITEM_KEY, itenName);
+                    networkList.add(items);
+                    adapter.notifyDataSetChanged();
+                }
+
+            }
+        }, new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
+    }
+
+
+    public void onClick(View view) {
+        networkList.clear();
+        wifi.startScan();
+        textStatus.setText("Scanning....");
     }
 
     @Override
