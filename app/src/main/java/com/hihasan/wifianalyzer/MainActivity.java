cb9dc098b1d8e760555a8f9a5e76584cff63f8b7
hihsan <hasan@hihasan.com>
2017-04-09 22:41:54 +0600

    Menu Setup
    Going to work on Access point & COntour Graph

--- app/src/main/java/com/hihasan/wifianalyzer/MainActivity.java
+++ app/src/main/java/com/hihasan/wifianalyzer/MainActivity.java
@@ -1,9 +1,6 @@
 package com.hihasan.wifianalyzer;
 
-import android.content.Context;
-import android.content.Intent;
 import android.net.wifi.ScanResult;
-import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,14 +9,9 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.Toast;
 
-import com.hihasan.wifianalyzer.contourgraph.ContourMain;
-
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
@@ -37,9 +29,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        //listview wifi
-        listViewWifi = (ListView) this.findViewById(R.id.listView_wifi);
-        mapScan = new HashMap<>();
+
 
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
@@ -51,44 +41,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
-        //wifimanager
-        WifiManager wifiMan = (WifiManager) this.getSystemService(Context.WIFI_SERVICE);
 
-        ArrayList<ScanResult> scanResults = (ArrayList<ScanResult>) wifiMan.getScanResults();
-        for (ScanResult scanResult : scanResults)
-        {
-
-            ArrayList<ScanResult> listScanResult;
-
-            if ((listScanResult = (ArrayList<ScanResult>) mapScan.get(scanResult.SSID)) == null) {
-                listScanResult = new ArrayList<>();
-                listScanResult.add(scanResult);
-                mapScan.put(scanResult.SSID, listScanResult);
-            } else {
-                listScanResult.add(scanResult);
-                mapScan.put(scanResult.SSID, listScanResult);
-            }
-        }
-
-        ScanResultAdapter adapter = new ScanResultAdapter(this, R.layout.row, scanResults);
-
-        listViewWifi.setAdapter(adapter);
-
-        adapter.notifyDataSetChanged();
-
-        listViewWifi.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            public void onItemClick(AdapterView<?> parent, View view,int position, long id) {
-
-                Object o = listViewWifi.getItemAtPosition(position);
-                ScanResult scanResult = (ScanResult) o;
-
-                Toast.makeText(getBaseContext(), "Nombre : "+mapScan.get(scanResult.SSID).size(), Toast.LENGTH_SHORT).show();
-            }
-        });
-
-
-//        WifiInfo wifiInf = wifiMan.getConnectionInfo();
-//        String macAddr = wifiInf.getMacAddress();
 
 
 
@@ -114,10 +67,15 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
 
 
-        if (id == R.id.nav_contour)
+        if (id == R.id.nav_access)
         {
-            Intent n=new Intent(MainActivity.this, ContourMain.class);
-            startActivity(n);
+            Toast.makeText(getApplicationContext(),"This is access Point",Toast.LENGTH_SHORT).show();
+        }
+
+        else if (id==R.id.nav_contour)
+        {
+            Toast.makeText(getApplicationContext(),"This is COntour",Toast.LENGTH_SHORT).show();
+
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
